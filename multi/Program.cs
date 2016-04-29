eb33b7766652caf857cde19a42f8e32818236c2c
宋强 <songqiang0521@163.com>
2016-04-29 12:46:26 +0800

    资源文件-多语言

---
+++ multi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace multi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ci = CultureInfo.CurrentCulture;
+            //var ci = new CultureInfo("zh-CN");
+            ResourceManager rm = new ResourceManager("multi.multi", Assembly.GetExecutingAssembly());
+            string msg = rm.GetString("hello", ci);
+            Console.WriteLine(msg);
+
+            rm = new ResourceManager("multi.multi2", Assembly.GetExecutingAssembly());
+            msg = rm.GetString("hello", ci);
+            Console.WriteLine(msg);
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+            Console.WriteLine(multi.hello);
+            Console.WriteLine(multi2.hello);
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("zh-CN");
+            Console.WriteLine(multi.hello);
+            Console.WriteLine(multi2.hello);//界面的显示和UI相关
+        }
+    }
+}
